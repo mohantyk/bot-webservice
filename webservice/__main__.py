1a3db47b19800b5d2fae8f552de7bcf06b6d417a
Kaniska Mohanty <mohantyk@users.noreply.github.com>
2019-08-01 05:59:00 -0700

    Add handler for issue opened event.

--- webservice/__main__.py
+++ webservice/__main__.py
@@ -8,13 +8,40 @@ Created on Thu Aug  1 05:26:25 2019
 
 import os
 
+import aiohttp
 from aiohttp import web
 
+from gidgethub import routing, sansio
+from gidgethub import aiohttp as gh_aiohttp
+
+router = routing.Router()
 routes = web.RouteTableDef()
 
-@routes.get("/")
+@router.register('issues', action='opened')
+async def issue_opened_event(event, gh, *args, **kwargs):
+    url = event.data['issue']['comments_url']
+    author = event.data['issue']['user']['login']
+    message = f"Thanks for the report @{author}! I will look into it ASAP! (I'm a bot)"
+    await gh.post(url, data={"body": message})
+
+@routes.post("/")
 async def main(request):
-    return web.Response(status=200, text="Hello World!")
+    # read the GitHub webhook payload
+    body = await request.read()
+    
+    # our authentication token and secret
+    secret = os.environ.get("GH_SECRET")
+    oauth_token = os.environ.get("GH_AUTH")
+    
+    # a representation of GitHub webhook event
+    event = sansio.Event.from_http(request.headers, body, secret=secret)
+    
+    async with aiohttp.ClientSession() as session:
+        gh = gh_aiohttp.GitHubAPI(session, 'mohantyk', ouath_token=oauth_token)
+        await router.dispatch(event, gh)
+        
+    return web.Response(status=200)
+
 
 if __name__ == '__main__':
     app = web.Application()
